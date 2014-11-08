60a38e4b875182707aeff26da7a977edfd568ebe
Olaf Wisselink <olafwisselink@live.nl>
2014-11-08 13:41:04 +0100

    Base class gemaakt

--- guitarfx/main.cpp
+++ guitarfx/main.cpp
@@ -6,7 +6,7 @@ enum{ARG_NAME = 0, ARG_LEVEL, NUM_ARGS};
 
 float newLevel;
 float buffer[] = {0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0, 0.5};
-float* tempbuffer;
+float* tempBuffer;
 
 int main(int argc, char* argv[]) {
 
@@ -18,16 +18,18 @@ int main(int argc, char* argv[]) {
   newLevel = atof(argv[ARG_LEVEL]);
 
   Amplifier amp;
-  cout << "Amplifier level: " << amp.showLevel() << endl;
 
   amp.setLevel(newLevel);
+  cout << "Amplifier level: " << amp.showLevel() << endl;
+
   amp.readSamples(buffer);
   amp.processSamples();
-  tempbuffer = amp.writeSamples();
+  tempBuffer = amp.writeSamples();
+
   for(int i = 0; i <10; i++) {
-    cout << tempbuffer[i] << endl;
+    cout << tempBuffer[i] << endl;
   }
 
+  //delete[] tempBuffer;
   return 0;
-
 } //Main
