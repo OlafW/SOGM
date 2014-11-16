b4d2d978f854f207127e7e53562ce5d36a57076e
Olaf Wisselink <olafwisselink@live.nl>
2014-11-16 22:42:58 +0100

    Added fake tremolo

---
+++ guitarfx/tremolo.cpp
@@ -0,0 +1,30 @@
+#include "tremolo.h"
+#include <math.h>
+
+
+Tremolo::Tremolo()
+{
+    Amplifier(); //Constructor
+    this->modDepth = 0.75;
+    this->modFreq = 0.5;
+}
+
+void Tremolo::setModFreq(float modFreq) {
+  this->modFreq = modFreq;
+}
+
+
+
+void Tremolo::setModDepth(float modDepth) {
+  this->modDepth = modDepth;
+}
+
+
+
+void Tremolo::process()
+{
+  cout << "Tremolo: " << "modDepth = " << modDepth << endl;
+  cout << "Tremolo: " << "modFreq = " << modFreq << endl;
+  cout << "Tremolo: " << "processing...." << endl;
+
+}
---
+++ guitarfx/tremolo.h
@@ -0,0 +1,19 @@
+#ifndef _TREMOLO_H_
+#define _TREMOLO_H_
+
+#include "amplifier.h"
+
+class Tremolo : public Amplifier {
+
+public:
+  Tremolo();
+  void setModFreq(float modFreq);
+  void setModDepth(float modDepth);
+  void process();
+
+private:
+  float modFreq;
+  float modDepth;
+}; //Tremolo class
+
+#endif
