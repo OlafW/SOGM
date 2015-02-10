cbf1e139b82fea873fd6b3b532f985163dc2cfb9
Olaf Wisselink <olafwisselink@live.nl>
2015-02-10 01:28:49 +0100

    fir analyse

---
+++ blok2/fir_analyse/fir.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <complex>
+#include <math.h>
+
+double freq = 0.0;
+double amp = 0.0;
+int count = 100;
+
+std::complex<double> i(0.0, 1.0);
+std::complex<double> z;
+
+
+double lowpass(double freq) {
+
+    z = std::exp(-i * freq);
+    amp = std::sqrt( std::pow(1 + std::real(z), 2.0) + std::pow( std::imag(z), 2.0) ) * 0.5;
+
+    return amp;
+}
+
+
+double highpass(double freq) {
+
+    z = std::exp(-i * freq);
+    amp = std::sqrt( std::pow(1 - std::real(z), 2.0) + std::pow( std::imag(z), 2.0) ) * 0.5;
+
+    return amp;
+}
+
+
+int main() {
+
+  for (int i = 0; i<count; i++) {
+
+      std::cout << lowpass(freq) << std::endl;
+      freq += (M_PI/count);
+  }
+
+return 0;
+}
+
+
+/*
+make fir
+fir -> firplot.txt
+gnuplot
+plot "firplot.txt" with lines
+*/
