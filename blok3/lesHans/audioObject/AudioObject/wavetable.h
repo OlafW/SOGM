dd041063804b62db43d503ac219d93ff0b61f5fd
Olaf Wisselink <olafwisselink@live.nl>
2015-06-08 11:35:11 +0200

    wavetable

---
+++ blok3/lesHans/audioObject/AudioObject/wavetable.h
@@ -0,0 +1,32 @@
+//
+//  wavetable.h
+//  WaveTable
+//
+//  Created by Olaf Wisselink on 21-05-15.
+//  Copyright (c) 2015 olaf. All rights reserved.
+//
+
+#ifndef __WaveTable__wavetable__
+#define __WaveTable__wavetable__
+
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+#define SIZE 4096   //Buffersize
+
+class WaveTable {
+public:
+    WaveTable();
+    WaveTable(long size);
+    ~WaveTable();
+    void init();
+    double linearInterp(double lookup);
+    
+private:
+    double* data;   //data of wavetable
+    long size;  //size of wavetable
+    int sampleRate;
+};
+
+#endif /* defined(__WaveTable__wavetable__) */
